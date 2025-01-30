3a4f39de06343b08215486a7f1eff7c5c5aecda3
Aidna8908 <117658827+Aidna8908@users.noreply.github.com>
2025-01-29 19:23:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example JavaScript functionality for any interactivity
+document.addEventListener("DOMContentLoaded", function() {
+    // You can add any interactivity or features here
+    const img = document.querySelector('img');
+    
+    img.addEventListener('click', () => {
+        alert('Image clicked!');
+    });
+});
